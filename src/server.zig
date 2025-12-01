7c6c0b36bed36e0718266bfab169ec8607271e95
Jacob-Walton <jacob-walton@konpeki.co.uk>
2025-12-01 19:07:04 +0000

    fix: content type mismatch

--- src/server.zig
+++ src/server.zig
@@ -205,49 +205,31 @@ fn contentType(path: []const u8) httpz.ContentType {
     const ext = std.fs.path.extension(path);
     if (ext.len < 2) return .BINARY;
 
-    // Check for .js first
+    // Check common types first
     if (std.mem.eql(u8, ext, ".js")) return .JS;
     if (std.mem.eql(u8, ext, ".css")) return .CSS;
     if (std.mem.eql(u8, ext, ".html")) return .HTML;
     if (std.mem.eql(u8, ext, ".htm")) return .HTML;
-
-    switch (ext.len) {
-        4 => switch (ext[1]) {
-            'p' => if (std.mem.eql(u8, ext, ".png")) return .PNG,
-            'g' => if (std.mem.eql(u8, ext, ".gif")) return .GIF,
-            'i' => if (std.mem.eql(u8, ext, ".ico")) return .ICO,
-            't' => if (std.mem.eql(u8, ext, ".ttf")) return .TTF,
-            'w' => if (std.mem.eql(u8, ext, ".wasm")) return .WASM,
-            'o' => if (std.mem.eql(u8, ext, ".otf")) return .OTF,
-            'e' => if (std.mem.eql(u8, ext, ".eot")) return .EOT,
-            else => {},
-        },
-        5 => switch (ext[1]) {
-            'c' => if (std.mem.eql(u8, ext, ".csv")) return .CSV,
-            'j' => if (std.mem.eql(u8, ext, ".json")) return .JSON,
-            'p' => if (std.mem.eql(u8, ext, ".pdf")) return .PDF,
-            't' => if (std.mem.eql(u8, ext, ".ttf")) return .TTF,
-            'w' => if (std.mem.eql(u8, ext, ".woff")) return .WOFF,
-            else => {},
-        },
-        6 => switch (ext[1]) {
-            'j' => if (std.mem.eql(u8, ext, ".jpeg")) return .JPG,
-            'w' => if (std.mem.eql(u8, ext, ".woff2")) return .WOFF2,
-            's' => if (std.mem.eql(u8, ext, ".svg")) return .SVG,
-            else => {},
-        },
-        7 => switch (ext[1]) {
-            't' => if (std.mem.eql(u8, ext, ".tar.gz")) return .GZ,
-            else => {},
-        },
-        else => {},
-    }
-
+    if (std.mem.eql(u8, ext, ".json")) return .JSON;
+    if (std.mem.eql(u8, ext, ".png")) return .PNG;
     if (std.mem.eql(u8, ext, ".jpg")) return .JPG;
+    if (std.mem.eql(u8, ext, ".jpeg")) return .JPG;
+    if (std.mem.eql(u8, ext, ".gif")) return .GIF;
+    if (std.mem.eql(u8, ext, ".svg")) return .SVG;
+    if (std.mem.eql(u8, ext, ".ico")) return .ICO;
+    if (std.mem.eql(u8, ext, ".woff")) return .WOFF;
+    if (std.mem.eql(u8, ext, ".woff2")) return .WOFF2;
+    if (std.mem.eql(u8, ext, ".ttf")) return .TTF;
+    if (std.mem.eql(u8, ext, ".otf")) return .OTF;
+    if (std.mem.eql(u8, ext, ".eot")) return .EOT;
+    if (std.mem.eql(u8, ext, ".wasm")) return .WASM;
+    if (std.mem.eql(u8, ext, ".pdf")) return .PDF;
+    if (std.mem.eql(u8, ext, ".csv")) return .CSV;
     if (std.mem.eql(u8, ext, ".gz")) return .GZ;
+    if (std.mem.eql(u8, ext, ".tar.gz")) return .GZ;
+    if (std.mem.eql(u8, ext, ".tar")) return .TAR;
     if (std.mem.eql(u8, ext, ".xml")) return .XML;
     if (std.mem.eql(u8, ext, ".txt")) return .TEXT;
-    if (std.mem.eql(u8, ext, ".tar")) return .TAR;
     if (std.mem.eql(u8, ext, ".webp")) return .WEBP;
 
     return .BINARY;
